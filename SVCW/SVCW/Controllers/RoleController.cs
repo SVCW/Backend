fa5be98e0fe5f530eb84b0194047028b31d13484
SE151245 <quyentvse151245@fpt.edu.vn>
2023-06-11 21:01:38 +0700

    commit

---
+++ SVCW/SVCW/Controllers/RoleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SVCW.DTOs.Roles;
+using SVCW.DTOs;
+using SVCW.Interfaces;
+using SVCW.Models;
+
+namespace SVCW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private IRole service;
+        public RoleController(IRole service)
+        {
+            this.service = service;
+        }
+
+        [Route("Insert-Category")]
+        [HttpPost]
+        public async Task<IActionResult> Insert(RoleCreateDTO dto)
+        {
+            ResponseAPI<List<Role>> responseAPI = new ResponseAPI<List<Role>>();
+            try
+            {
+                responseAPI.Data = await this.service.create(dto);
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+            }
+        }
+    }
+}
---
+++ SVCW/SVCW/Services/RoleService.cs
@@ -0,0 +1,64 @@
+﻿using SVCW.DTOs.Roles;
+using SVCW.Interfaces;
+using SVCW.Models;
+
+namespace SVCW.Services
+{
+    public class RoleService : IRole
+    {
+        private readonly SVCWContext _context;
+        public RoleService(SVCWContext context)
+        {
+            _context = context;
+        }
+        public async Task<Role> create(RoleCreateDTO role)
+        {
+            try
+            {
+                var rle = new Role();
+                rle.Description = role.Description;
+                rle.RoleName = role.RoleName;
+                rle.RoleId = "ROLE" + Guid.NewGuid().ToString().Substring(0,6);
+                rle.Status = true;
+
+                await this._context.Role.AddAsync(rle);
+                if(await this._context.SaveChangesAsync() > 0)
+                {
+                    return rle;
+                }
+                else
+                {
+                    throw new Exception("Save DB Fail");
+                }
+            }catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Task<Role> delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> findById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Role>> findByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Role>> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> update(RoleUpdateDTO role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

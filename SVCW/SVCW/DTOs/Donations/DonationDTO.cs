f0c0f5a4b01407f27b18a0449c0a46527105ffb7
SE151245 <quyentvse151245@fpt.edu.vn>
2023-06-13 16:57:13 +0700

    code

--- SVCW/SVCW/DTOs/Donations/DonationDTO.cs
+++ SVCW/SVCW/DTOs/Donations/DonationDTO.cs
@@ -1,6 +1,20 @@
-﻿namespace SVCW.DTOs.Donations
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SVCW.DTOs.Donations
 {
     public class DonationDTO
     {
+        public string DonationId { get; set; }
+        public string Title { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Datetime { get; set; }        
+        public string Email { get; set; }        
+        public string Phone { get; set; }       
+        public string Name { get; set; }
+        public bool? IsAnonymous { get; set; }        
+        public string Status { get; set; }
+        public string ActivityId { get; set; }
     }
 }

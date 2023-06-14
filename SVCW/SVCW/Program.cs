a677e2aa85ac36fb649cb8427d9271512baf2af0
uocneuocday <83950328+uocnguyenngoc3010@users.noreply.github.com>
2023-06-14 23:35:49 +0700

    updateCommentApi

--- SVCW/SVCW/Program.cs
+++ SVCW/SVCW/Program.cs
@@ -1,6 +1,8 @@
+﻿using Microsoft.OpenApi.Models;
 using SVCW.Interfaces;
 using SVCW.Models;
 using SVCW.Services;
+using System.Reflection;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,9 @@ options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoop
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IRole, RoleService>();
+builder.Services.AddScoped<IAchivement, AchivementService>();
+builder.Services.AddScoped<IReportType, ReportTypeService>();
+builder.Services.AddScoped<IProcessType, ProcessTypeService>();
 
 
 
@@ -22,17 +27,37 @@ builder.Services.AddScoped<IRole, RoleService>();
 
 builder.Services.AddScoped(typeof(SVCWContext));
 
+builder.Services.AddAuthorization();
+
+
+builder.Services.AddEndpointsApiExplorer();
+//cmt cho api
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("SVCW", new OpenApiInfo() { Title = "SVCW", Version = "v1" });
+    //setup comment in swagger UI
+    var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentFileFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+
+    option.IncludeXmlComments(xmlCommentFileFullPath);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/SVCW/swagger.json", "SVCWApi v1"));
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
+//mở API cho FE kết nối
+app.UseCors(x => x.AllowAnyOrigin()
+                 .AllowAnyHeader()
+                 .AllowAnyMethod());
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

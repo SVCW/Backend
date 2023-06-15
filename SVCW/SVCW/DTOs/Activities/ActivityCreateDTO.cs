4bd8e8624c26c8adeecf687f5679c47f57cf22d1
SE151245 <quyentvse151245@fpt.edu.vn>
2023-06-16 01:50:36 +0700

    update thêm bảng mà Quyền quên

---
+++ SVCW/SVCW/DTOs/Activities/ActivityCreateDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SVCW.DTOs.Activities
+{
+    public class ActivityCreateDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Location { get; set; }
+        public decimal? TargetDonation { get; set; }
+        public string UserId { get; set; }
+        public bool isFanpageAvtivity { get; set; }
+    }
+}

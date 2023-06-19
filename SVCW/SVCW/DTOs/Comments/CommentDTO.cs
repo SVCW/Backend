989f5cda347aa324a09bb894c63de060bd8271d3
Phat Huynh <phatst1232@gmail.com>
2023-06-19 15:37:21 +0700

    Update Comment APIs

---
+++ SVCW/SVCW/DTOs/Comments/CommentDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SVCW.DTOs.Comments
+{
+	public class CommentDTO
+	{
+        public string UserID { get; set; }
+        public string ActivityId { get; set; }
+        public string CommentContent { get; set; }
+    }
+}
+

7e2ce4bc970d7141c25dec8f0646037b5eecb5d5
SE151245 <quyentvse151245@fpt.edu.vn>
2023-06-29 16:30:11 +0700

    alllllllll

---
+++ SVCW/SVCW/Interfaces/IConfig.cs
@@ -0,0 +1,11 @@
+﻿using SVCW.DTOs.Config;
+
+namespace SVCW.Interfaces
+{
+    public interface IConfig
+    {
+        adminConfig GetAdminConfig();
+        adminConfig updateAdminConfig(adminConfig update);
+        userCreateActivityConfig getConfig(string userId);
+    }
+}

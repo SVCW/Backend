65b28a06c8672ceff716a6837d8601322f10441e
Phat Huynh <84378037+phatst1232@users.noreply.github.com>
2023-07-08 21:52:57 +0700

    Revert "merge main"

---
+++ SVCW/SVCW/DTOs/Config/userCreateActivityConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace SVCW.DTOs.Config
+{
+    public class userCreateActivityConfig
+    {
+        public bool isDonatable { get; set; }
+        public bool isFanpage { get; set; }
+        public bool isValidCreate { get; set; }
+        public string? message { get; set; }
+        public float maxDonate { get; set; }
+    }
+}
